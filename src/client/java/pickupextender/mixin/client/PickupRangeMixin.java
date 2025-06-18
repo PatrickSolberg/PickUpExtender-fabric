54660099f0f2178f68a82120984eb4f78de2e6b8
patrick <patrick.oysteinsen@gmail.com>
2025-06-18 16:38:01 +0200

    Add PickupRangeMixin to enhance item pickup range based on configuration settings

---
+++ src/client/java/pickupextender/mixin/client/PickupRangeMixin.java
@@ -0,0 +1,40 @@
+package pickupextender.mixin.client;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pickupextender.config.ModConfig;
+
+import java.util.List;
+
+@Mixin(ItemEntity.class)
+public abstract class PickupRangeMixin extends Entity {
+    @Shadow public abstract void onPlayerCollision(PlayerEntity player);
+
+    public PickupRangeMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at = @At("TAIL"), method = "tick")
+    private void tick(CallbackInfo ci) {
+        int radius = Math.max(1, ModConfig.getInstance().pickupRange);
+        Vec3d itemPos = this.getPos();
+        BlockPos blockPos = new BlockPos((int) itemPos.getX(), (int) itemPos.getY(), (int) itemPos.getZ());
+        Box box = new Box(blockPos).expand(radius);
+        List<PlayerEntity> players = this.getWorld().getNonSpectatingEntities(PlayerEntity.class, box);
+
+        if (!players.isEmpty()) {
+            this.onPlayerCollision(players.get(0));
+        }
+    }
+} 
